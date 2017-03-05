8dfa6d01c2eeac2970d36a2b3401243053f17e84
Nathan Willson <nathanwillson@Nathans-MacBook-Air.local>
2017-03-04 18:16:19 -0800

    name in english

--- server.rb
+++ server.rb
@@ -64,8 +64,8 @@ post '/start' do
   	return {
     	color: MODE == "B" ? "#1869DF" : "#00FF00",
     	head_url: "https://i.makeagif.com/media/9-26-2016/jFT1D0.gif",
-    	name: "ゴロゴロ",
-    	taunt: "swish",
+    	name: "gorogoro",
+    	taunt: "ゴロゴロ",
       head_type: "sand-worm",
       tail_type: "curled"
   	}.to_json
